3dfad2e32c01e3a90f35c922221b8ccd40f6747a
RodionLaz <s89972@nhs.co.il>
2024-03-07 16:52:57 +0200

    init

---
+++ Front/list/src/components/list.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+
+
+
+function List() {
+
+    const getList = () => {
+
+    }
+
+  return (
+    <div className="list container">
+        <h1>LIST</h1>
+        <div>
+        <button>Get LIST</button>
+        <button>Update List</button>
+        <button>Remove List</button>
+        </div>
+        <ul>
+        {}
+        </ul>
+
+    </div>
+  );
+}
+
+export default List;
